acbfc751701842db29fa2fbe808306c6ea01b30f
l8110 <2392493944@qq.com>
2022-06-08 13:07:42 +0800

    第三次提交

--- js/02.js
+++ js/02.js
@@ -7,13 +7,3 @@ $('.qd').click(function() {
 	$('.box4').css("display", "none");
 	$('.box5').css("display", "none");
 })
-var x = 3;
-$('.ks').click(function () {
-	x--;
-	if (x<0) {
-		x=3;
-		$('.obx').css("display", "block");
-		$('.box5').css("display", "block");
-	}
-	$('b').html(x);
-})
